37fc61dc6116c9ea3c1fb1f9de7be19167c3f952
Aditya Ghidora <adityaghidora@gmail.com>
2025-09-28 00:07:53 +0530

    refactor: Add a file reader class
    Added a file reader class to encapsulate the file reading logic
    in a single class.

---
+++ src/core/file_reader.py
@@ -0,0 +1,31 @@
+from src.models import AppConfig, Line, SourceFile
+
+
+class FileReader:
+    """Reads file contents from disk into SourceFile objects."""
+
+    def __init__(self, app_config: AppConfig):
+        self.app_config = app_config
+
+    def read_all(self) -> list[SourceFile]:
+        """
+        Reads a list of files from disk.
+
+        Returns:
+            A list of SourceFile objects.
+        """
+        files = []
+
+        for filename in self.app_config.filenames:
+            comment_lines = []
+
+            with open(filename, mode="r") as f:
+                for idx, line in enumerate(f.readlines()):
+                    if line.startswith(self.app_config.comment_symbol):
+                        comment_lines.append(Line(line_number=idx + 1, content=line))
+
+            file = SourceFile(path=filename, lines=comment_lines)
+
+            files.append(file)
+
+        return files
--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from typing import List, Optional
 class CopyrightStatus(StrEnum):
     """Defines the possible states of a copyright notice."""
 
-    UP_TO_DATE = auto()
+    OK = auto()
     OUTDATED = auto()
     MISSING = auto()
 
@@ -23,18 +23,25 @@ class AppConfig:
 
 
 @dataclass
+class Line:
+    """Represents a single line in a source file."""
+
+    line_number: int
+    content: str
+
+
+@dataclass
 class SourceFile:
     """Represents a single source file's identity and full content."""
 
     path: str
-    lines: List[str]
+    lines: List[Line]
 
 
 @dataclass
 class AnalysisResult:
     """Provides a detailed report on the copyright status of one file."""
 
-    source_file: SourceFile
+    file: SourceFile
     status: CopyrightStatus
-    line_index: Optional[int] = None
-    existing_start_year: Optional[int] = None
+    line_number: Optional[int] = None
